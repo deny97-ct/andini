3a1236e85673ecac776099bc2c6f60f9d232ee4f
deny97-ct <deny97@gmail.com>
2026-03-04 16:14:43 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Pastikan baris ini ada agar gaya Tailwind CSS Anda termuat
 
+// Kode di bawah ini mencari elemen <div id="root"> di dalam file index.html
+// lalu "menyuntikkan" seluruh komponen App kita ke dalamnya.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
