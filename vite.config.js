fc55dd15df3dc3e0f2af50f4860aace727a13d3d
deny97-ct <deny97@gmail.com>
2026-03-04 16:05:26 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Pastikan tidak ada properti 'base' di sini untuk deployment Vercel
+})
